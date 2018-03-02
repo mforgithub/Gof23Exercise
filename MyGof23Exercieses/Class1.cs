10a01ea73f9b6581f9045d4295eae5c8bb61faf5
mforgithub <mforwow@126.com>
2018-03-02 15:18:07 +0800

    第一次服务器提交测试

---
+++ MyGof23Exercieses/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGof23Exercieses
+{
+    class Class1
+    {
+    }
+}
